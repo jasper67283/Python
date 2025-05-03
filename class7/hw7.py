459bd0c820150298d615e60907164dc0471c9e48
jasper67283 <jasperkuo07170717@gmail.com>
2025-05-03 10:50:02 +0800

    hw7

--- class7/hw7.py
+++ class7/hw7.py
@@ -1,9 +1,11 @@
+# 不能輸入23以內的數字
 f = int(input("請輸入數字1: "))
 t = int(input("請輸入數字2: "))
-for i in range(f, t + 1):
-    for r in range(2, 23 + 1):
-        if i % r == 0:
-            print(0)
-            break
-    else:
-        print(i)
+for n in range(f, t + 1):
+    if n > 1:
+        ans = "是質數"
+        for i in range(2, n):
+            if n % i == 0:
+                ans = "不是質數"
+    if ans == "是質數":
+        print(n)
